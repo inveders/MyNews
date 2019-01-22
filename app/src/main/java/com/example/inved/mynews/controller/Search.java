b5a80999900cf6869b6e91b739abf9942ca32f80
inveders <alexandra.gnimadi@gmail.com>
2019-01-22 23:57:53 +0100

    Working on the TDD Search.

--- app/src/main/java/com/example/inved/mynews/controller/Search.java
+++ app/src/main/java/com/example/inved/mynews/controller/Search.java
@@ -1,14 +1,47 @@
 package com.example.inved.mynews.controller;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.example.inved.mynews.R;
+
+import static android.content.ContentValues.TAG;
+
 public class Search {
 
-    private double mBalance;
+    private CheckBox checkboxTechnology, checkboxScience, checkboxSports, checkboxFood, checkboxTravel, checkboxWorld ;
 
-    public Search(double initialBalance){
-        mBalance= initialBalance ;
-    }
+    Button buttonSearch;
 
-    public double getBalance(){
-        return mBalance;
-    }
+
+
+  /*  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.search, container, false);
+
+        checkboxTechnology = v.findViewById(R.id.checkBox_technology);
+        checkboxScience = v.findViewById(R.id.checkBox_science);
+        buttonSearch = v.findViewById(R.id.button_Search);
+
+
+        checkboxTechnology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkboxTechnology.isChecked(); //yourCheckBox.isChecked() is the method to know if the checkBox is checked
+                Log.d(TAG, "onClick: yourCheckBox = ");
+
+                if (((CheckBox) v).isChecked()) {
+                    //Toast.makeText(SearchTest.this, "Checked", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+    }*/
 }
--- app/src/test/java/com/example/inved/mynews/SearchTest.java
+++ app/src/test/java/com/example/inved/mynews/SearchTest.java
@@ -1,17 +1,45 @@
 package com.example.inved.mynews;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.example.inved.mynews.controller.MainActivity;
 import com.example.inved.mynews.controller.Search;
 
 import org.junit.Test;
 
+import static android.content.ContentValues.TAG;
 import static org.junit.Assert.assertEquals;
 
 public class SearchTest {
 
-    @Test
+   /* @Test
     public void createWallet() throws Exception {
         Search wallet = new Search(42);
 
         assertEquals(42, wallet.getBalance(), 0.001);
-    }
+    }*/
+
+
+    private CheckBox checkboxTechnology;
+
+        @Test
+        public void isCheckboxTechnologyChecked(){
+           // CheckBox checkboxTechnology = (CheckBox)v;
+            if (checkboxTechnology.isChecked()){
+                Log.d("DEBAGoa","OK");
+            }
+        }
+
+
+
+
 }
