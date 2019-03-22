3b8f6559577c62e8fd925a690089bad91bc55413
inveders <alexandra.gnimadi@gmail.com>
2019-03-22 11:13:06 +0100

    Organization of the xml files (notification and search) for re-use them with the includes #2.

---
+++ app/src/main/java/com/example/inved/mynews/controller/NotificationActivity.java
@@ -0,0 +1,17 @@
+package com.example.inved.mynews.controller;
+
+import android.os.Bundle;
+
+import com.example.inved.mynews.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
